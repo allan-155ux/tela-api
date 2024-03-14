04146767c4b017707820bfbe9e39bf28322f54f8
allan.155ux <allanmg155@gmail.com>
2024-03-14 09:42:26 -0300

    1,2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const db = require('./db.js')
 const app = express();
 const cors = require('cors')
-const port = 3001 || process.env.PORT;
+const port = process.env.PORT || 3001;
 
 // Middleware para permitir o parsing de JSON
 app.use(express.json());
